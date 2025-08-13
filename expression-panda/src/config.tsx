32d4d5358978c5c327bb512928e343975030f6a9
MrBao <bnguyen4656@gmail.com>
2025-08-13 18:56:18 -0500

    Hide middlewareUri

---
+++ expression-panda/src/config.tsx
@@ -0,0 +1,2 @@
+export const MIDDLEWARE_URI = import.meta.env.VITE_MIDDLEWARE_URI as string;
+export const clientId = import.meta.env.VITE_CLIENT_ID as string;
--- expression-panda/src/main.tsx
+++ expression-panda/src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import { App } from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const clientId = import.meta.env.VITE_CLIENT_ID as string;
-console.log(clientId);
+import { clientId } from "./config";
+
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={clientId}>
     <App />
